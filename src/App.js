32636c76949f29aaa0cf5b89c7ae5cc90f4817cc
Daniel Karl <danielkarl888@gmail.com>
2022-04-01 16:37:54 +0300

    1st commit test for React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container-fluid">
+    <div class="row">
+      <div class="col-2">
+
+      </div>
+      <div class="col card mt-2" id="conncectCard">
+        <div class="form-floating mb-3 input-padding-5">
+          <input type="text" class="form-control mt-2 " id="floatingInput" placeholder="Username"></input>
+          <label for="floatingInput">Username</label>
+        </div>
+        <div class="form-floating mb-3 input-padding-5">
+          <input type="password" class="form-control mt-2" id="floatingPassword" placeholder="Password"></input>
+          <label for="floatingPassword">Password</label>
+        </div>
+        <div class="form-floating" id="addRegister">
+          <input type="text" class="form-control mt-2" id="displayName" placeholder="Display name"></input>
+          <label for="displayName">Display name</label>
+        </div>
+        <div class="row" id="rowLogin">
+          <div class="col-11 input-padding-5 p-3">
+            <button type="submit" class="btn btn-primary btn-karl" id="login-btn">Login</button>
+            <span class="p-3">not registered? <a onmouseover="console.log(10)" href="" onclick="console.log(5)">Click
+                here</a> to register</span>
+          </div>
+        </div>
+        <div class="row" id="rowRegister">
+          <div class="col-11 input-padding-5 p-3">
+            <button type="submit" class="btn btn btn-primary btn-karl" id="register-btn">Register</button>
+            <span class="p-3">Already registered? <a onmouseover="console.log(10)" href=""
+                onclick="console.log(5)">Click here</a> to login</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-2">
+      </div>
     </div>
+  </div>
+
+
   );
 }
 
