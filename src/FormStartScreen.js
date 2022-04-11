512f77d415ac891c352226afedc4e82265af9ee9
dvirlan <dvir37@gmail.com>
2022-04-11 16:21:45 +0300

    router added

--- src/FormStartScreen.js
+++ src/FormStartScreen.js
@@ -4,11 +4,20 @@ import React from 'react';
 import {useState} from 'react';
 import RegisterPage from './RegisterPage';
 import MainView from './ChatView/mainView';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 function FormStartScreen () {
 
         return ( 
          
-          <MainView/>
+          <> 
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<LoginPage/>}></Route>
+              <Route path="/regi" element={<RegisterPage/>}></Route>
+              <Route path="/chat" element={<MainView/>}></Route>
+            </Routes>
+          </BrowserRouter>
+          </>
           
         );
 }
