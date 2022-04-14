59cf28ea290001fd58168c272dbc5bcc04af065f
danielkarl888 <danielkarl888@gmail.com>
2022-04-14 18:03:01 +0300

    added basic modal from bootstrap

--- src/ChatView/Left/TopHeadingLeft.js
+++ src/ChatView/Left/TopHeadingLeft.js
@@ -1,7 +1,7 @@
 import activeUser from "../../ManagingUsersList/activeUser";
 function TopHeadingLeft() {
     return (
-      
+
         <div className="container-fluid row heading">
             <div className="col-sm-2 col-xs-2 heading-avatar">
                 <div className="heading-avatar-icon">
@@ -9,12 +9,20 @@ function TopHeadingLeft() {
                 </div>
             </div>
             <div className="col-sm-8 headingUsername">
-                <span className="name-meta" style={{ marginLeft: "40%"}}>{activeUser.display}
+                <span className="name-meta" style={{ marginLeft: "40%" }}>{activeUser.display}
                 </span>
             </div>
             <div className="col-sm-2 col-xs-2 heading-compose">
-               <button className="btn-bg-transparent hiddenbtn"> <i className="bi bi-person-plus-fill" aria-hidden="true" style={{ marginLeft: "40%" }}></i></button>
-               
+                <button type="button" className="btn-bg-transparent hiddenbtn" data-toggle="modal" data-target=".bd-example-modal-lg"> <i className="bi bi-person-plus-fill" aria-hidden="true" style={{ marginLeft: "40%" }}></i></button>
+
+                <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+                            ...
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
