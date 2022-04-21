5a4a37010e5c551f88210526b9d863a72596eb3f
dvirlan <dvir37@gmail.com>
2022-04-21 14:01:05 +0300

    modal add new contact final

--- src/ChatView/Left/AddNewContact.js
+++ src/ChatView/Left/AddNewContact.js
@@ -11,64 +11,77 @@ import { DropdownButton } from "react-bootstrap";
 
 
 function AddNewContact({ conversationsActiveUser, setConversationsActiveUser }) {
-
     
     const handleAdd = (event) => {
+        if(newConversation.username!="" && userNamesConver[userNamesConver.length-1]!=newConversation.username){
         event.preventDefault();
-        const update = [...conversationsActiveUser,newConversation ];
+        const update = [...conversationsActiveUser, newConversation];
         setConversationsActiveUser(update);
-        console.log(conversationsActiveUser);
-
+        console.log(conversationsActiveUser);}
     }
 
     const handleAddChange = (event) => {
+        if(event.target.value!=""){
         console.log(event.target.value);
-        setNewConversation({username:event.target.value,messages:[{ src: "", type: "", context: "", time: "" }]})
+        
+
+        setNewConversation({ username: event.target.value, messages: [{ src: "", type: "", context: "", time: "" }] })
+        console.log(userNamesConver);
+        }
 
     }
 
     const [newConversation, setNewConversation] = useState({
-
         username: "",
         messages: [{ src: "", type: "", context: "", time: "" }]
     });
 
     const userNamesConver = [];
     for (let index = 0; index < conversationsActiveUser.length; index++) {
-        userNamesConver.push(conversationsActiveUser[index].username);        
+        userNamesConver.push(conversationsActiveUser[index].username);
     }
 
 
 
     return (
-        <form className="modal fade" id="addModal" tabIndex="-1" role="dialog" aria-labelledby="addModal" aria-hidden="true">
-            <div className="modal-dialog modal-dialog-centered" role="document">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title">Add new contact</h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div className="modal-body">
-                        <select 
-                        onChange={handleAddChange}
-                            class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                            <option selected>select new contact</option>
-                            {userList.map((value, index) => {
-                                if (activeUser.userName != value.userName && !(userNamesConver.includes(value.userName))) {
-                                    return <option>
-                                     <li key={index}>{value.userName}</li></option>
-                                }
-                            })}
-                            
-                           </select>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" onClick={handleAdd} className="btn btn-success btn-sm"  style={{ width: "20%" }}>Add</button>
-                    </div>
-                </div>
+        <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Add new contact</h5>
+              <button type="button" className="btn-close" data-dismiss="modal"></button>
+
             </div>
-        </form>
+            <div class="modal-body">
+            <select
+            onChange={handleAddChange}
+                class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                <option value="" selected >select new contact</option>
+                
+                {userList.map((value, index) => {
+                   
+                    if (activeUser.userName != value.userName && !(userNamesConver.includes(value.userName))) {
+    
+                        return <option>
+                            <li key={index}>{value.userName}</li></option>
+                    }
+                })}
+      
+            </select>
+       
+            </div>
+            <div class="modal-footer">
+            <button type="button" onClick={handleAdd} className="btn btn-success btn-sm" data-dismiss="modal" style={{ width: "20%" }}>Add</button>
+              </div>
+          </div>
+        </div>
+      </div>
     );
 }
 
 export default AddNewContact;          
+
+
+
+
+
