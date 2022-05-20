965473e310c29ae830caa872b083ba5a67c77e08
danielkarl888 <danielkarl888@gmail.com>
2022-05-20 12:55:14 +0300

    added link to the ranks page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import './App.css';
 import FormStartScreen from './FormStartScreen';
 function App() {
       return (
+      <>     
+
       <div className="container-fluid">
       <div className="row">
+      
         <div className="col-1">
         </div>
         <FormStartScreen/>
         <div className="col-1">
+
+        <a class="btn btn-success btn-sm m-3 " href="http://localhost:5121/Ranks/index" role="button">Rank Us! <i class="bi bi-heart-fill"></i>
+</a>
+
         </div>
+
       </div>
+
     </div>
+
+    </>
     );
 }
 export default App;
