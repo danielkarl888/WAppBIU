9e6edc08d4c28f8e3eea498221590a8494e9e1e5
dvirlan <dvir37@gmail.com>
2022-05-22 15:32:15 +0300

    outgoing messages fixed design

--- src/ChatView/Right/Messages.js
+++ src/ChatView/Right/Messages.js
@@ -3,15 +3,13 @@ function Message({context, time}) {
     
     return(
         <div className="message">
-    <div className="mes-content">
-        <div className="mes-text">
-            {context}
-        </div>
+            <div className="mes-content">
+                <div className="mes-text">
+                {context}
+                </div>
         <div className="mes-text-time">{time}</div>
-
-
-    </div>
-</div>  
+            </div>
+        </div>  
     );
    
 }
--- src/ChatView/Right/OutComeMess.js
+++ src/ChatView/Right/OutComeMess.js
@@ -1,14 +1,25 @@
 function OutComeMessage({context, time}) {
     
     return(
-        <div className="Outmessage position-relative">
-    <div className="Outmes-content position-absolute top-0 end-0">
-        <div className="mes-text">
-            {context}
+        <div class="row message">
+        <div class="col-sm-12 message-main-sender">
+          <div class="sender">
+            <div class="mes-text">
+             {context}
+            </div>
+            <div class="mes-text-time">
+              {time}
+            </div>
+          </div>
         </div>
-        <div className="mes-text-time">{time}</div>
-    </div>
-</div>  
+      </div>
+        
+        
+
+
+
+
+        
     );
 }
 export default OutComeMessage
